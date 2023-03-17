316072ba2d35b40a6ca8ffdf4a060520bc084502
Souhardya <souhardyamalakar.cob@gmail.com>
2023-03-18 02:02:19 +0530

    hmm

--- dist/index.js
+++ dist/index.js
@@ -27,7 +27,7 @@ const http_1 = require("http");
 const socket_io_1 = require("socket.io");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true,
 }));
 app.use(body_parser_1.default.json());
