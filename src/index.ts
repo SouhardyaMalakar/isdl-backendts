1a97e7cf3caddd2b8aef4c4f8f74a0c2544463c3
Souhardya <souhardyamalakar.cob@gmail.com>
2023-03-18 04:07:43 +0530

    weird

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,14 @@ app.use(
     credentials: true,
   })
 );
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+});
+
+
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(UserRouter);
